f23b53470718fdbcb32b60ba58a0c5f4ad116d27
yulog <29723448+yulog@users.noreply.github.com>
2024-06-23 11:26:07 +0900

    refactor: 一部でgoqueryを使わずhtml.Nodeを使うように変更

--- general.go
+++ general.go
@@ -12,6 +12,7 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"github.com/otiai10/opengraph/v2"
 	"github.com/yulog/go-favicon"
+	xhtml "golang.org/x/net/html"
 )
 
 type General struct{}
@@ -30,10 +31,10 @@ func (*General) summarize(s *Summaly) (Summary, error) {
 	doc := goquery.NewDocumentFromNode(s.Node)
 	doc.Url = s.URL // URLをセット(oEmbedで使う)
 
-	m := meta(doc)
+	var m = &info{}
+	m.walk(s.Node)
 
-	title := cmp.Or(ogp.Title, m.TwitterTitle, doc.Find("title").Text())
-
+	title := cmp.Or(ogp.Title, m.Twitter.Title, m.Title)
 	title = Clip(html.UnescapeString(title), 100)
 
 	icons, err := favicon.New(favicon.NopSort, favicon.IgnoreWellKnown).FindGoQueryDocument(doc, s.URL.String())
@@ -71,7 +72,7 @@ func (*General) summarize(s *Summaly) (Summary, error) {
 		icon = icons[0].URL
 	}
 
-	description := cmp.Or(ogp.Description, m.TwitterDescription, m.Description)
+	description := cmp.Or(ogp.Description, m.Twitter.Description, m.MetaInfo.Description)
 	description = Clip(html.UnescapeString(description), 300)
 
 	if title == description {
@@ -81,19 +82,22 @@ func (*General) summarize(s *Summaly) (Summary, error) {
 	image := ""
 	if len(ogp.Image) > 0 {
 		image = ogp.Image[0].URL
-	} else if m.TwitterImage != "" {
-		image = m.TwitterImage
-		// } else if v := doc.Find("link[rel='image_src']").AttrOr("href", ""); v != "" {
-		// 	image = v
-		// } else if v := doc.Find("link[rel='apple-touch-icon']").AttrOr("href", ""); v != "" {
-		// 	image = v
-		// } else if v := doc.Find("link[rel='apple-touch-icon image_src']").AttrOr("href", ""); v != "" {
-		// 	image = v
-		// }
-	} else if v, exists := link(doc); exists {
-		// もとのような優先順位がなくなり、順不同で初めに見つかったものを採用
-		image = v
+	} else if m.Twitter.Image != "" {
+		image = m.Twitter.Image
+	} else {
+		image = cmp.Or(m.LinkImage.ImageSrc, m.LinkImage.AppleTouchIcon, m.LinkImage.AppleTouchIconImageSrc)
 	}
+	// } else if v := doc.Find("link[rel='image_src']").AttrOr("href", ""); v != "" {
+	// 	image = v
+	// } else if v := doc.Find("link[rel='apple-touch-icon']").AttrOr("href", ""); v != "" {
+	// 	image = v
+	// } else if v := doc.Find("link[rel='apple-touch-icon image_src']").AttrOr("href", ""); v != "" {
+	// 	image = v
+	// }
+	// } else if v, exists := link(doc); exists {
+	// 	// もとのような優先順位がなくなり、順不同で初めに見つかったものを採用
+	// 	image = v
+	// }
 
 	if image != "" {
 		u, err := url.Parse(image)
@@ -106,7 +110,7 @@ func (*General) summarize(s *Summaly) (Summary, error) {
 		}
 	}
 
-	sitename := cmp.Or(ogp.SiteName, m.ApplicationName, s.URL.Host)
+	sitename := cmp.Or(ogp.SiteName, m.MetaInfo.ApplicationName, s.URL.Host)
 	sitename = html.UnescapeString(strings.TrimSpace(sitename))
 
 	title = CleanupTitle(title, sitename)
@@ -138,6 +142,89 @@ func (*General) summarize(s *Summaly) (Summary, error) {
 	}, nil
 }
 
+func (m *info) walk(n *xhtml.Node) {
+	if n.Type == xhtml.ElementNode {
+		switch n.Data {
+		case "title":
+			title := opengraph.TitleTag(n)
+			m.Title = title.Text
+		case "link":
+			link := opengraph.LinkTag(n)
+			switch link.Rel {
+			case "image_src":
+				m.LinkImage.ImageSrc = link.Href
+			case "apple-touch-icon":
+				m.LinkImage.AppleTouchIcon = link.Href
+			case "apple-touch-icon image_src":
+				m.LinkImage.AppleTouchIconImageSrc = link.Href
+			}
+		case "meta":
+			meta := opengraph.MetaTag(n)
+			prop := cmp.Or(meta.Property, meta.Name)
+			if prop != "" && meta.Content != "" {
+				switch prop {
+				case "twitter:title":
+					if m.Twitter.Title == "" {
+						m.Twitter.Title = meta.Content
+					}
+				case "twitter:description":
+					if m.Twitter.Description == "" {
+						m.Twitter.Description = meta.Content
+					}
+				case "description":
+					if m.MetaInfo.Description == "" {
+						m.MetaInfo.Description = meta.Content
+					}
+				case "twitter:image":
+					if m.Twitter.Image == "" {
+						m.Twitter.Image = meta.Content
+					}
+				case "twitter:card":
+					if m.Twitter.Card == "" {
+						m.Twitter.Card = meta.Content
+					}
+				case "twitter:player":
+					if m.Twitter.Player == "" {
+						m.Twitter.Player = meta.Content
+					}
+				case "twitter:player:width":
+					if m.Twitter.PlayerWidth == "" {
+						m.Twitter.PlayerWidth = meta.Content
+					}
+				case "twitter:player:height":
+					if m.Twitter.PlayerHeight == "" {
+						m.Twitter.PlayerHeight = meta.Content
+					}
+				case "application-name":
+					if m.MetaInfo.ApplicationName == "" {
+						m.MetaInfo.ApplicationName = meta.Content
+					}
+				}
+			}
+		}
+	}
+
+	for child := n.FirstChild; child != nil; child = child.NextSibling {
+		m.walk(child)
+	}
+}
+
+func _linkImage(doc *goquery.Document) string {
+	var l1, l2, l3 string
+	doc.Find("link").Each(func(i int, s *goquery.Selection) {
+		rel, _ := s.Attr("rel")
+		switch rel {
+		case "image_src":
+			l1, _ = s.Attr("href")
+		case "apple-touch-icon":
+			l2, _ = s.Attr("href")
+		case "apple-touch-icon image_src":
+			l3, _ = s.Attr("href")
+		}
+	})
+	return cmp.Or(l1, l2, l3)
+}
+
 func link(doc *goquery.Document) (val string, exists bool) {
 	doc.Find("link").EachWithBreak(func(i int, s *goquery.Selection) bool {
 		rel, _ := s.Attr("rel")
@@ -146,13 +233,30 @@ func link(doc *goquery.Document) (val string, exists bool) {
 			val, exists = s.Attr("href")
 			return false
 		}
-		// val, exists = "", false
 		return true
 	})
 	return
 }
 
+type info struct {
+	Title     string
+	MetaInfo  metaInfo
+	Twitter   twitter
+	LinkImage linkImage
+}
+
+type linkImage struct {
+	ImageSrc               string
+	AppleTouchIcon         string
+	AppleTouchIconImageSrc string
+}
+
 type metaInfo struct {
+	Description     string
+	ApplicationName string
+}
+
+type metaInfo_ struct {
 	TwitterTitle        string
 	TwitterDescription  string
 	Description         string
@@ -164,7 +268,17 @@ type metaInfo struct {
 	ApplicationName     string
 }
 
-func meta(doc *goquery.Document) (m metaInfo) {
+type twitter struct {
+	Title        string
+	Description  string
+	Image        string
+	Card         string
+	Player       string
+	PlayerWidth  string
+	PlayerHeight string
+}
+
+func meta(doc *goquery.Document) (m metaInfo_) {
 	doc.Find("meta").EachWithBreak(func(i int, s *goquery.Selection) bool {
 		prop, _ := s.Attr("property")
 		name, _ := s.Attr("name")
@@ -220,22 +334,22 @@ func meta(doc *goquery.Document) (m metaInfo) {
 }
 
 // getPlayer は Twitter/X, OGP の *Player を返す
-func getPlayer(m metaInfo, ogp *opengraph.OpenGraph) *Player {
+func getPlayer(m *info, ogp *opengraph.OpenGraph) *Player {
 	var playerUrl string
 	var playerWidth any
 	var playerHeight any
 
 	// Twitter/X Player
-	if m.TwitterCard != "summary_large_image" && m.TwitterPlayer != "" {
-		playerUrl = m.TwitterPlayer
+	if m.Twitter.Card != "summary_large_image" && m.Twitter.Player != "" {
+		playerUrl = m.Twitter.Player
 	}
 
-	if m.TwitterPlayerWidth != "" {
-		playerWidth, _ = strconv.Atoi(m.TwitterPlayerWidth)
+	if m.Twitter.PlayerWidth != "" {
+		playerWidth, _ = strconv.Atoi(m.Twitter.PlayerWidth)
 	}
 
-	if m.TwitterPlayerHeight != "" {
-		playerHeight, _ = strconv.Atoi(m.TwitterPlayerHeight)
+	if m.Twitter.PlayerHeight != "" {
+		playerHeight, _ = strconv.Atoi(m.Twitter.PlayerHeight)
 	}
 
 	// OGP Player
